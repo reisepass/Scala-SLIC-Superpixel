4f4da1efe95650a1ddc18a510e69182ac12f960f
reisepass <rubenwolff@gmail.com>
2015-12-24 17:01:52 -0500

    small comment change

--- src/main/scala/ch/ethz/dalab/scalaslic/starter.scala
+++ src/main/scala/ch/ethz/dalab/scalaslic/starter.scala
@@ -11,18 +11,17 @@ object starter {
   def main(args: Array[String]): Unit = {
 
     if (args.length < 2) {
-      println("Incorrect number of arguments");
-      println(""); //TODO specify which params mean what 
+      println("Incorrect number of arguments"); 
       throw new Exception;
     }
 
     val fileLoc = args(0);
     val superpixelSize = args(1).toInt;  
     val compactness = ((args.lift(2)).getOrElse(Double.MinValue.toString())).toDouble
-    val minBlob = (args.lift(3)).getOrElse("-1").toInt
+    val minSuperPixelSize = (args.lift(3)).getOrElse("-1").toInt
     val imageData:Array[Array[Array[Int]]] =utils.fileReadling(fileLoc);
     
-    val slicworker = new SLICgreySimple(imageData,superpixelSize,compactness,myMinBlob=minBlob);
+    val slicworker = new SLICgreySimple(imageData,superpixelSize,compactness,myMinSize=minSuperPixelSize);
     val superPixelMask= slicworker.calcSuperPixels();
     
     
@@ -30,7 +29,7 @@ object starter {
     val origFile = opener.openImage(fileLoc);
     println(new File(".").getAbsolutePath()); 
     
-    utils.printSuperPixels(superPixelMask,origFile,300.0,new File(fileLoc).getName()+"(S_"+superpixelSize+") (M_"+compactness+") (MinSupSiz_"+minBlob+")");
+    utils.printSuperPixels(superPixelMask,origFile,300.0,new File(fileLoc).getName()+"(S_"+superpixelSize+") (M_"+compactness+") (MinSupSiz_"+minSuperPixelSize+")");
     println("Done.");
   }
 }
